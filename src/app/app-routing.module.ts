f8d8b03e50390e2ca4f17878db52006b182de3cb
zb <8761804@iesplayamar.es>
2024-09-19 04:33:31 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './layout-page/layout-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LayoutPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutPageComponent } from './layout-page/layout-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutPageComponent
   ],
   imports: [
     BrowserModule,
