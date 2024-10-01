dd67890a21261c7ee8565db8466a38f6fb2ed163
zb <8761804@iesplayamar.es>
2024-10-01 17:59:25 +0200

    Primer formato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutPageComponent } from './layout-page/layout-page.component';
+import { IntroComponent } from './secciones/intro/intro.component';
+import { PortfolioComponent } from './secciones/portfolio/portfolio.component';
+import { ContactComponent } from './secciones/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutPageComponent
+    LayoutPageComponent,
+    IntroComponent,
+    PortfolioComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
