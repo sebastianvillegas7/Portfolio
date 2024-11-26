706d05824494c4f4aa997e339f5f0b2855be753c
zb <8761804@iesplayamar.es>
2024-11-26 16:19:26 +0100

    estilos carruseles

--- src/app/secciones/portfolio/portfolio.component.ts
+++ src/app/secciones/portfolio/portfolio.component.ts
@@ -12,7 +12,9 @@ export class PortfolioComponent {
     { src: '../../../assets/img/tecs/angular.png', alt: 'Angular' },
     { src: '../../../assets/img/tecs/material.png', alt: 'Angular Material' },
     { src: '../../../assets/img/tecs/SpringBoot.png', alt: 'Spring Boot' },
+    { src: '../../../assets/img/tecs/intellij.png', alt: 'IntelliJ' },
     { src: '../../../assets/img/tecs/oracle.png', alt: 'Oracle' },
+    { src: '../../../assets/img/tecs/discogs.png', alt: 'Discogs API' },
     { src: '../../../assets/img/tecs/javascript.png', alt: 'JavaScript' },
     { src: '../../../assets/img/tecs/mysql.png', alt: 'MySQL' },
     { src: '../../../assets/img/tecs/vsc.png', alt: 'Visual Studio Code' },
@@ -21,5 +23,13 @@ export class PortfolioComponent {
 
   showProject(projectNumber: number): void {
     this.selectedProject = projectNumber;
+  }
+
+  groupImages(images: any[], groupSize: number): any[][] {
+    const groups = [];
+    for (let i = 0; i < images.length; i += groupSize) {
+      groups.push(images.slice(i, i + groupSize));
+    }
+    return groups;
   }
 }
